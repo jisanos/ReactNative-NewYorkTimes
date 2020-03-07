b55141f4eb38e5ce73d8783251459a572139000b
jisanos <44531911+jisanos@users.noreply.github.com>
2020-03-07 12:37:58 -0400

    Added onSubmitEditing to search query when done is pressed

--- App/Components/News.js
+++ App/Components/News.js
@@ -14,31 +14,31 @@ import { material } from 'react-native-typography' //consider using this!
 import { Metrics, Colors } from '../Themes'
 
 export default class News extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
   }
 
   render() {
 
     return (
-      <TouchableHighlight onPress= {this.props.method}>
-      <View>
-        <Text style={styles.itemTitle}>
-          {this.props.item.title}
-        </Text>
+      <TouchableHighlight onPress={this.props.method}>
+        <View>
+          <Text style={styles.itemTitle}>
+            {this.props.item.title}
+          </Text>
 
-        <Text style={styles.itemText}>
-          {this.props.item.snippet}
-        </Text>
+          <Text style={styles.itemText}>
+            {this.props.item.snippet}
+          </Text>
 
-        <Text style={styles.itemAuthor}>
-          {this.props.item.byline}
-        </Text>
+          <Text style={styles.itemAuthor}>
+            {this.props.item.byline}
+          </Text>
 
-        <Text style={styles.itemID}>
-          {this.props.item.date}
-        </Text>
-      </View>
+          <Text style={styles.itemID}>
+            {this.props.item.date}
+          </Text>
+        </View>
       </TouchableHighlight>
     );
   }
@@ -51,16 +51,16 @@ const styles = StyleSheet.create({
   },
   itemText: {
     fontSize: 14,
-    
+
   },
   itemID: {
     paddingBottom: 20,
     fontSize: 12,
-    color:'#999999',
-    
+    color: '#999999',
+
   },
-  itemAuthor:{
-    fontWeight:'500',
+  itemAuthor: {
+    fontWeight: '500',
     fontSize: 14,
   },
   itemTitle: {
